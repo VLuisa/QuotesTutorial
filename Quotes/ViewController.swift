b34d8c3a2eaa4244fecab2bd4e4ba6873095b0bd
Luisa <luisavasquez@Luisas-MacBook-Pro.local>
2017-12-12 11:22:40 -0500

    Initial Commit

---
+++ Quotes/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Quotes
+//
+//  Created by Luisa Vasquez on 12/12/17.
+//  Copyright © 2017 DALI. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
